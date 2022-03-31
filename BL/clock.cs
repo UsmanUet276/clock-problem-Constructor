fbfc594abdb71ba7b26a62297086c8567c2d6175
Muhammad Usman Bin Farid <99407553+UsmanUet276@users.noreply.github.com>
2022-03-31 18:04:33 +0500

    Add files via upload

---
+++ BL/clock.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task.BL
+{
+    internal class clock
+    {
+        public clock()
+        {
+        }
+        public clock(int hours)
+        {
+            this.hours = hours;
+        }
+        public clock(int minutes, int hours)
+        {
+            this.minutes = minutes;
+            this.hours = hours;
+        }
+        public clock(int second, int minutes, int hours)
+        {
+            this.seconds = second;
+            this.minutes = minutes;
+            this.hours = hours;
+        }
+
+        public int seconds;
+        public int minutes;
+        public int hours;
+
+        public void incrementInSeconds()
+        {
+            seconds++;
+        }
+        public void incrementInMinutes()
+        {
+            minutes++;
+        }
+        public void incrementInHour()
+        {
+            hours++;
+        }
+        public void printObject()
+        {
+            Console.WriteLine(hours + " , " + minutes + " , " + seconds);
+        }
+        public bool manualTimingInput(int hours , int minutes , int seconds)
+        {
+            if(hours == this.hours && minutes == this.minutes && seconds == this.seconds)
+            {
+                return true;
+            }
+            return false;   
+        }
+        public bool objectTimingInput(clock temp)
+        {
+            if (hours == temp.hours && minutes == temp.minutes && seconds == temp.seconds)
+            {
+                return true;
+            }
+            return false ;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task.BL;
+
+namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tf = false;
+            clock a = new clock();
+            clock b = new clock(8);
+            clock c = new clock(8, 10);
+            clock d = new clock(8, 10, 10);
+            clock e = new clock(8, 10, 10);
+
+            
+            Console.Write("Empty Time : ");
+            a.printObject();
+            
+            Console.Write("Hour time : ");
+            b.printObject();
+            
+            Console.Write("Minute Time : ");
+            c.printObject();
+            
+            Console.Write("Second time : ");
+            d.printObject();
+
+            Console.Write("Full Time(Increment Seconds) : ");
+            d.incrementInSeconds();
+            d.printObject();
+
+            Console.Write("Full Time(Increment Minutes) : ");
+            d.incrementInMinutes();
+            d.printObject();
+
+            Console.Write("Full Time(Increment Hours) : ");
+            d.incrementInHour();
+            d.printObject();
+
+            Console.Write("Flag : ");
+            tf = d.manualTimingInput(9, 11, 11);
+            Console.WriteLine(tf);
+
+            Console.Write("Object Flag : ");
+            tf = d.objectTimingInput(e);
+            Console.WriteLine(tf);
+            Console.ReadKey();
+        }
+    }
+}
